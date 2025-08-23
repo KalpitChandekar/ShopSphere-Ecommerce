7490cd8f6c67d11350c925c726df154f8ae6d285
Kalpit <kalpitchandekar1736@gmail.com>
2025-08-23 14:52:36 +0530

    feat: setup Navbar

---
+++ Components/Navbar.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+export const Navbar = () => {
+  const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setMobileOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <nav className="sticky top-0 z-50 bg-white shadow">
+      <div className="container mx-auto flex items-center justify-between px-4 py-4">
+        <Link href="/" className="hover:text-blue-600">
+          ShopSphere
+        </Link>
+        <div className="hidden md:flex space-x-6">
+          <Link href="/">Home</Link>
+          <Link href="/products" className="hover:text-blue-600">
+            Products
+          </Link>
+          <Link href="/checkout" className="hover:text-blue-600">
+            Checkout
+          </Link>
+        </div>
+      </div>
+      {mobileOpen && (
+        <nav className="md:hidden bg-white shadow-md">
+          <ul className="flex flex-col p-4 space-y-2">
+            <li>
+              <Link href="/" className="block hover:text-blue-600">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/products" className="block hover:text-blue-600">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link href="/checkout" className="block hover:text-blue-600">
+                Checkout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </nav>
+  );
+};
