97ed5224edeb7aab874cf8edf4a999b22097a178
Kalpit <kalpitchandekar1736@gmail.com>
2025-08-26 13:52:42 +0530

    feat: replace $ to ₹

--- Components/Carousel.tsx
+++ Components/Carousel.tsx
@@ -43,7 +43,7 @@ export const Carousel = ({ products }: Props) => {
         </CardTitle>
         {price && price.unit_amount && (
           <p className="text-xl text-white">
-            Rs. {(price.unit_amount / 100).toFixed(2)}
+            ₹{(price.unit_amount / 100).toFixed(2)}
           </p>
         )}
       </CardContent>
